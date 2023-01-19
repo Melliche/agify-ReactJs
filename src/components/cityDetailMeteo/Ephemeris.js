a216399c14e88cd24b9845b36000c6fd14a861a7
Julick Mellah <j.mellah@icloud.com>
2023-01-19 16:49:39 +0100

    avant MUI

--- src/components/cityDetailMeteo/Ephemeris.js
+++ src/components/cityDetailMeteo/Ephemeris.js
@@ -1,4 +1,15 @@
+import METEO from "../../api/meteoAPI";
+
+
 export default function Ephemeris ({insee}) {
+
+  const Ephemeris = METEO.getCityEphemerisByInsee(insee).then((data) => {
+    console.log(data);
+    return data;
+  });
+  console.log(Ephemeris);
+
+
   return (
     <section className="ephemeris">
       <h1>Éphémérides</h1>
